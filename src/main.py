680cba49a81279e0bf73ddedebcb310e10cb059f
mcquenji <benjamin.mceachnie@necode.it>
2024-09-21 12:24:35 +0000

    feat: use docker instead of running moodle locally

--- src/main.py
+++ src/main.py
@@ -2,53 +2,55 @@ import sys
 import os
 import click
 from click_spinner import spinner
-from setup import install_moodle, get_moodle_versions, get_repo
-from moodle import MOODLE_PATH
-import inquirer 
-
-if not os.path.exists(MOODLE_PATH):
-    click.echo('Initializing...')
-    with spinner():
-        get_repo()
+import driver
+import moodle as mdl
+import inquirer
 
 if sys.version_info[0:2] != (3, 12):
-    raise Exception('Requires python 3.12')
+    raise Exception("Requires python 3.12")
 
 
-    
 @click.group(invoke_without_command=True)
 @click.pass_context
 def main(ctx: click.Context) -> None:
     if ctx.invoked_subcommand is None:
         click.echo(ctx.get_help())
-        
+
+
 @main.command()
-@click.argument('version', type=click.Choice(get_moodle_versions()), required=False)
-def init(version: str|None) -> None:
+@click.argument("version", type=click.Choice(mdl.get_releases()), required=False)
+def init(version: str | None) -> None:
     """
     Downloads and sets up a local moodle instance.
     """
-    
+
     if version is None:
-        choice = inquirer.prompt([
-            inquirer.List('version', message='Select moodle version', choices=get_moodle_versions())
-        ])
-        
-        version = choice['version']
-    
-    click.echo(f'Installing moodle version {version}...')
-    with spinner():
-        install_moodle(version)
-        
-    
+        choice = inquirer.prompt(
+            [
+                inquirer.List(
+                    "version",
+                    message="Select moodle version",
+                    choices=mdl.get_releases(),
+                )
+            ]
+        )
+
+        version = choice["version"]
+
+    config = driver.Config(version)
+    driver.save_config(config)
+
+
 @main.command()
 def run() -> None:
     """
     Runs the moodle instance.
     """
-    click.echo('Running moodle...')
-    pass
-    
-if __name__ == '__main__':
-    main.no_args_is_help = True;
+    click.echo("Running moodle...")
+    with spinner():
+        driver.launch_moodle()
+
+
+if __name__ == "__main__":
+    main.no_args_is_help = True
     main()
--- src/moodle.py
+++ src/moodle.py
@@ -1,8 +1,32 @@
-MOODLE_PATH = '.moodle'
+import requests as r
+from packaging.version import parse
 
-def launch_moodle():
-    """
-    Launches the moodle instance, prints the url to the console and takes a snapshot of the current state after termination.
-    """
-    # TODO(RiedleroD): Implement this function
-    pass
+
+# def get_releases(depth: int = 1) -> list[str]:
+#     tags = []
+#     url = "https://hub.docker.com/v2/repositories/bitnami/moodle/tags?page_size=100"
+#     page = 0
+
+#     while url is not None and page < depth:
+#         response = r.get(url)
+#         response.raise_for_status()
+#         data = response.json()
+
+#         tags.extend(
+#             tag["name"]
+#             for tag in data["results"]
+#             if not tag["name"].startswith("sha")
+#             and "debian" not in tag["name"]
+#             and tag["name"] != "latest"
+#         )
+
+#         url = data.get("next")
+#         page += 1
+
+#     sorted_tags = sorted(tags, key=parse, reverse=True)
+
+#     return ["latest"] + sorted_tags
+
+
+def get_releases(depth: int = 1) -> list[str]:
+    return ["latest", "4.3.7", "3.11.11"]
