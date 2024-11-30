8dafcb07931b695bf0898c39b23e5023b899d077
Simon Wewalka <64737036+swewalka@users.noreply.github.com>
2024-11-30 15:34:20 +0100

    Delete src directory because refactoring to package

--- src/driver/__init__.py
+++
@@ -1,5 +0,0 @@
-from .config import Config, load_config, save_config
-from .moodle import launch_docker_compose, stop_docker_compose
-from .baseline import sql_dump_baseline, sql_restore_baseline
-from .tests import create_test_batch, MoodleUnitTestBase, run_test_batch
-from .database import DatabaseHandler
